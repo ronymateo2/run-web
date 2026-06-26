d79939cc12613741f843a114c9fa3531c1f0be61
Rony Mateo <rony.mateo@ext.abcdin.cl>
2026-06-26 14:16:43 -0500

    clean cleanupOutdatedCaches

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
 			workbox: {
 				skipWaiting: true,
 				clientsClaim: true,
+				cleanupOutdatedCaches: true,
 				globPatterns: ["**/*.{js,css,html,ico,png,svg,wasm,mp3}"],
 				runtimeCaching: [
 					{
